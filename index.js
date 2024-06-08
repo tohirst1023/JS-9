a885fb7e98cde46c167f6770b6c5158fe9bb99db
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-06-08 15:07:08 +0500

    sdgsgea

---
+++ index.js
@@ -0,0 +1,14 @@
+let letterSearch = ['non', 'asal', 'banan', 'olma', 'aziza', 'Gul', 'mashina',]
+let nbor = []
+let nyoq = []
+
+for (let narsa of letterSearch) {
+    if(narsa.includes("n")){
+        nbor.push(narsa)
+    }else {
+         nyoq.push (narsa) 
+    }
+}
+
+console.log (nbor)
+console.log (nyoq)
